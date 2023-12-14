da5bf9219cf590674302ade4feab09105436c9d3
sahil <91470287+sahilthapa001@users.noreply.github.com>
2023-12-14 18:02:56 +0545

    toastify and tailwind integration

---
+++ backendd/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middleware/error");
+const app = express();
+
+const appRouter = require("./controller");
+const LWPError = require("./utils/error");
+
+app.use(
+	cors({
+		origin: ["http://localhost:5173"],
+		credentials: true,
+	})
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+	res.send("Home");
+});
+
+app.get("/test", (req, res) => {
+	res.send(typeof req.query.extention);
+});
+
+app.use("/api/v1", appRouter);
+
+app.use(errorMiddleware);
+
+module.exports = app;
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./reducers/user";
 
 const store = configureStore({
-	reducer: {
-		user: userSlice.reducer,
-		//seller;
-	},
+  reducer: {
+    user: userSlice.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
