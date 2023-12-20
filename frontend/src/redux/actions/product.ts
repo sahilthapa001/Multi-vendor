00e2d93b786b8369dc65ef077e318f7ee9818d82
sahil <91470287+sahilthapa001@users.noreply.github.com>
2023-12-20 19:08:26 +0545

    registration and ui of dashboard

---
+++ frontend/src/redux/actions/product.ts
@@ -0,0 +1,83 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import lwpAxios from "../../config/axiosConfig";
+import { AxiosError } from "axios";
+
+interface ShopLoginData {
+	name?: string;
+	email: string;
+	password: string;
+	address?: string;
+	phoneNumber?: string;
+	zipCode?: string;
+	rememberMe?: boolean;
+}
+
+export const shopLoginAsync = createAsyncThunk(
+	"shop/login",
+	async (loginData: ShopLoginData) => {
+		try {
+			const response = await lwpAxios.post("/shop/login", loginData, {
+				withCredentials: true,
+			});
+			return response.data;
+		} catch (error: unknown) {
+			if (error instanceof AxiosError) {
+				throw new Error("Login failed: " + error.response?.data.message);
+			} else {
+				return Promise.reject();
+			}
+		}
+	}
+);
+
+export const createShopAsync = createAsyncThunk(
+	"shop/create",
+	async (loginData: ShopLoginData) => {
+		try {
+			const response = await lwpAxios.post("/shop/create", loginData, {
+				withCredentials: true,
+			});
+			return response.data;
+		} catch (error: unknown) {
+			if (error instanceof AxiosError) {
+				throw new Error("Create failed: " + error.response?.data.message);
+			} else {
+				return Promise.reject();
+			}
+		}
+	}
+);
+
+export const activateShopAsync = createAsyncThunk(
+	"shop/active",
+	async (token: string) => {
+		try {
+			const response = await lwpAxios.get(`/shop/activation/${token}`);
+			return response.data;
+		} catch (error: unknown) {
+			if (error instanceof AxiosError) {
+				throw new Error("Login failed: " + error.response?.data.message);
+			} else {
+				return Promise.reject();
+			}
+		}
+	}
+);
+
+export const shopAutoLoginAsync = createAsyncThunk(
+	"shop/autoLogin",
+	async () => {
+		try {
+			const response = await lwpAxios.get("/shop", {
+				withCredentials: true,
+			});
+			return response.data;
+		} catch (error: unknown) {
+			if (error instanceof AxiosError) {
+				throw new Error("Login failed: " + error.response?.data.message);
+			} else {
+				return Promise.reject();
+			}
+		}
+	}
+);
--- frontend/src/redux/reducers/shop.ts
+++ frontend/src/redux/reducers/shop.ts
@@ -1,76 +1,89 @@
 import { createSlice } from "@reduxjs/toolkit";
 import {
-  shopLoginAsync,
-  createShopAsync,
-  activateShopAsync,
+	shopLoginAsync,
+	createShopAsync,
+	activateShopAsync,
+	shopAutoLoginAsync,
 } from "../actions/shop";
 
 interface Shop {
-  _id: string;
-  name: string;
-  email: string;
+	_id: string;
+	name: string;
+	email: string;
 }
 interface ShopState {
-  loading: "idle" | "pending" | "succeeded" | "failed";
-  isAuthenticated: boolean;
-  error: string | null;
-  shop: Shop | null;
+	loading: "idle" | "pending" | "succeeded" | "failed";
+	isAuthenticated: boolean;
+	error: string | null;
+	shop: Shop | null;
 }
 
 const initialState: ShopState = {
-  loading: "idle",
-  isAuthenticated: false,
-  error: null,
-  shop: null,
+	loading: "idle",
+	isAuthenticated: false,
+	error: null,
+	shop: null,
 };
 
 const shopSlice = createSlice({
-  name: "shop",
-  initialState: initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(shopLoginAsync.pending, (state) => {
-        state.loading = "pending";
-        state.error = null;
-      })
-      .addCase(shopLoginAsync.fulfilled, (state, action) => {
-        state.loading = "succeeded";
-        state.isAuthenticated = true;
-        state.shop = action.payload.user;
-      })
-      .addCase(shopLoginAsync.rejected, (state, action) => {
-        state.loading = "failed";
-        state.error = action.error.message || "An error occurred";
-        throw action.error;
-      })
-      .addCase(createShopAsync.pending, (state) => {
-        state.loading = "pending";
-        state.error = null;
-      })
-      .addCase(createShopAsync.fulfilled, (state) => {
-        state.loading = "succeeded";
-      })
-      .addCase(createShopAsync.rejected, (state, action) => {
-        state.loading = "failed";
-        state.error = action.error.message || "An error occurred";
-        throw action.error;
-      })
-      .addCase(activateShopAsync.pending, (state) => {
-        state.loading = "pending";
-        state.error = null;
-      })
-      .addCase(activateShopAsync.fulfilled, (state, action) => {
-        state.loading = "succeeded";
-        state.isAuthenticated = true;
-        state.shop = action.payload.user;
-      })
-      .addCase(activateShopAsync.rejected, (state, action) => {
-        state.loading = "failed";
-        state.error = action.error.message || "An error occurred";
-        throw action.error;
-      });
-  },
+	name: "shop",
+	initialState: initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(shopLoginAsync.pending, (state) => {
+				state.loading = "pending";
+				state.error = null;
+			})
+			.addCase(shopLoginAsync.fulfilled, (state, action) => {
+				state.loading = "succeeded";
+				state.isAuthenticated = true;
+				state.shop = action.payload.user;
+			})
+			.addCase(shopLoginAsync.rejected, (state, action) => {
+				state.loading = "failed";
+				state.error = action.error.message || "An error occurred";
+				throw action.error;
+			})
+			.addCase(createShopAsync.pending, (state) => {
+				state.loading = "pending";
+				state.error = null;
+			})
+			.addCase(createShopAsync.fulfilled, (state) => {
+				state.loading = "succeeded";
+			})
+			.addCase(createShopAsync.rejected, (state, action) => {
+				state.loading = "failed";
+				state.error = action.error.message || "An error occurred";
+				throw action.error;
+			})
+			.addCase(activateShopAsync.pending, (state) => {
+				state.loading = "pending";
+				state.error = null;
+			})
+			.addCase(activateShopAsync.fulfilled, (state, action) => {
+				state.loading = "succeeded";
+				state.isAuthenticated = true;
+				state.shop = action.payload.user;
+			})
+			.addCase(activateShopAsync.rejected, (state, action) => {
+				state.loading = "failed";
+				state.error = action.error.message || "An error occurred";
+				throw action.error;
+			})
+			.addCase(shopAutoLoginAsync.pending, (state) => {
+				state.loading = "pending";
+			})
+			.addCase(shopAutoLoginAsync.fulfilled, (state, action) => {
+				state.loading = "succeeded";
+				state.isAuthenticated = true;
+				state.shop = action.payload.user;
+			})
+			.addCase(shopAutoLoginAsync.rejected, (state) => {
+				state.loading = "failed";
+				// throw action.error;
+			});
+	},
 });
 
 export default shopSlice;
