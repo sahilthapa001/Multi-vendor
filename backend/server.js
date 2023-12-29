8727bb47c6b10411ff4010f892640a21ee4434f0
sahil <91470287+sahilthapa001@users.noreply.github.com>
2023-12-29 15:52:34 +0545

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
 const loadEnv = require("dotenv");
 const http = require("http");
-const app = require("../clone/multi-vendor/backend/app");
+const app = require("./app");
 const db = require("./db/db");
 const configureSocket = require("./socket");
 
 loadEnv.config({
-	path: "config/.env",
+  path: "config/.env",
 });
 
 db.connectDatabase();
 
 const httpServer = http
-	.createServer(app)
-	.listen(process.env.PORT, process.env.HOST, () => {
-		console.log(
-			`Server is running on http://${process.env.HOST}:${process.env.PORT}`
-		);
-	});
+  .createServer(app)
+  .listen(process.env.PORT, process.env.HOST, () => {
+    console.log(
+      `Server is running on http://${process.env.HOST}:${process.env.PORT}`
+    );
+  });
 
 configureSocket(httpServer);
 
 // If any un-handled exceptions then we will shut down the server
 process.on("unhandledRejection", (err) => {
-	console.log(`Shutting down the server for ${err.message}`);
-	console.log(`shutting down the server for unhandle promise rejection`);
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
 
-	httpServer.close(() => {
-		process.exit(1);
-	});
+  httpServer.close(() => {
+    process.exit(1);
+  });
 });
